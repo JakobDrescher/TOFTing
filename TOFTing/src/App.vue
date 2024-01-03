7541208a9273de1af85597993c9f2aac60eebc51
flo1a <f.pluhar@gmx.at>
2024-01-03 13:40:57 +0100

    Erster Prototyp des Designs

--- TOFTing/src/App.vue
+++ TOFTing/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import Achievement from './components/Achievement.vue'
 </script>
 
 <template>
+    <Achievement/>
 </template>
 
 <style scoped>
---
+++ TOFTing/src/components/Achievement.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="flex h-screen items-center max-w-[100%]">
+    <div class="flex flex-wrap flex-col grow mx-[5%] rounded-lg border-2 border-[#CC0000] bg-[#333333]">
+        <div class="flex justify-start items-center m-[5%]">
+            <img src="../assets/achievement_1_TOFTing_Dragan.png" width="100vw">
+            <div class="text-[30pt] ml-[5%]">Erfolg</div>
+        </div>
+        <div class="flex flex-col mx-[20%] text-[#CCCCCC]">
+            <div class="text-[25px]">Abteilung</div>
+            <div class="text-[18px]">I n</div>
+        </div>
+        <div class="flex mt-[3vw] mb-[5vw] flex-col items-center">
+        <hr width="70%">
+        </div>
+        <div class="flex flex-col mx-[20%] text-[#CCCCCC]">
+            <div class="text-[25px]">Abteilung</div>
+            <div class="text-[18px]">Information</div>
+        </div>
+        <div class="flex mt-[3vw] mb-[5vw] flex-col items-center">
+        <hr width="70%">
+        </div>
+        <div class="flex flex-col flex-wrap mx-[20%] text-[#CCCCCC]">
+            <div class="text-[25px]">Abteilung</div>
+            <div class="text-[18px]">Informat dg vbfhdbchbdchvdhcv dvcgdvhcvhdsvchdv chdsvchdshcjh</div>
+        </div>
+        <div class="flex mt-[3vw] mb-[5vw] flex-col items-center">
+        <hr width="70%">
+        </div>
+        <button class="mx-[30%] mt-[5%] mb-[10%] bg-[#FFFFFF] text-[#333333] py-[2%] rounded-md border-2 border-[#CC0000]">
+            Hauptseite
+        </button>
+    </div>
+    </div>
+</template>
+
+<style>
+    body{
+        background-image: url("../assets/Hintergrund.png");
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: fixed;
+    }
+</style>

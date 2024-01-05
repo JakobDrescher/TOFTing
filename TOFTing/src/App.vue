e3069abf8f251fbd887442d4a5b6321584978967
PatrickChe <pchellakudam@gmail.com>
2024-01-05 16:41:24 +0100

    design 95% fertig

--- TOFTing/src/App.vue
+++ TOFTing/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import MainPage from '@/assets/MainPage.vue';
 </script>
 
 <template>
+    <MainPage></MainPage>
 </template>
 
 <style scoped>
--- TOFTing/tailwind.config.js
+++ TOFTing/tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'alegreya-sans-sc': ['Alegreya Sans SC', 'sans-serif'],
+        'alegreya-sans-sc-bold': ['Alegreya Sans SC Bold', 'sans-serif'],
+        'alegreya-sans-sc-extrabold': ['Alegreya Sans SC Extrabold', 'sans-serif'],
+      },
+      fontSize: {
+        'xxs': '.75rem', // Adjust the size as needed
+      },
+    },
   },
   plugins: [],
 }

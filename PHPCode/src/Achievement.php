1989064b53044f1e807134cebd1a0522dde9c611
JakobDrescher <jakobdrescher6@gmail.com>
2024-01-06 15:37:55 +0100

    added achievement class

---
+++ PHPCode/src/Achievement.php
@@ -0,0 +1,85 @@
+<?php
+declare(strict_types=1);
+
+namespace Jdres\Tofting;
+const BASE_PATH = __DIR__ . "/../";
+require BASE_PATH . '/vendor/autoload.php';
+
+/**
+ * A class to store achievement data
+ */
+class Achievement
+{
+    private int $id;
+    private string $name;
+    private string $pictureFilename;
+    private Department $department;
+    private Location $location;
+    private string $description;
+
+    /**
+     * @param int $id
+     * @param string $name
+     * @param string $pictureFilename
+     * @param Department $department
+     * @param Location $location
+     * @param string $description
+     */
+    public function __construct(int $id, string $name, string $pictureFilename, Department $department, Location $location, string $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->pictureFilename = $pictureFilename;
+        $this->department = $department;
+        $this->location = $location;
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPictureFilename(): string
+    {
+        return $this->pictureFilename;
+    }
+
+    /**
+     * @return Department
+     */
+    public function getDepartment(): Department
+    {
+        return $this->department;
+    }
+
+    /**
+     * @return Location
+     */
+    public function getLocation(): Location
+    {
+        return $this->location;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}

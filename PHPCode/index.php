4b41049a6e285beee670ddc455952ef1196f2e6c
JakobDrescher <jakobdrescher6@gmail.com>
2024-01-06 15:50:33 +0100

    wrote some quick and simple tests to see if the classes work

---
+++ PHPCode/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Jdres\Tofting\User;
+use Jdres\Tofting\Department;
+use Jdres\Tofting\Location;
+use \Jdres\Tofting\Achievement;
+
+$testUser = new User("testUser");
+$testDepartment = new Department(1, "Informationstechnologie");
+$testLocation = new Location("1", "PH (157)", "Physiksaal");
+$testAchievement = new Achievement(1, "Wissenschaftler", "achievement_2_TOFTing_Dragan.png", $testDepartment, $testLocation, "Höchst wissenschaftliche Wörter");
+
+var_dump($testUser);
+var_dump($testAchievement);

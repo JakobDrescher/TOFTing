2095c8bf0e79a255f881709fbab2c21d7a30a84a
flo1a <105285535+flo1a@users.noreply.github.com>
2024-01-09 16:16:42 +0100

    fetch für alle Elmemente

--- TOFTing/src/components/Achievement.vue
+++ TOFTing/src/components/Achievement.vue
@@ -1,31 +1,28 @@
 <template>
-    <div v-if="achievement">
-        <p>{{ achievement.icon }}</p>
-    </div>
     <div class="flex h-screen items-center max-w-[100%]">
         <div class="flex flex-wrap flex-col grow mx-[5%] rounded-3xl border-2 border-[#CC0000] bg-[#333333]">
             <img class="mt-[-44pt] self-center" src="../assets/Masköttchen_looking_over_TOFTing_Dragan.png" width="200vw">
             <div class="flex justify-start items-center m-[5%]">
                 <img v-if="achievement" :src="achievement.icon" width="100vw">
-                <div class="text-[30pt] ml-[5%]">Erfolg</div>
+                <div v-if="achievement" class="text-[30pt] ml-[5%]">{{ achievement.name }}</div>
             </div>
             <div class="flex flex-col mx-[20%] text-[#CCCCCC]">
                 <div class="text-[25px]">Abteilung</div>
-                <div class="text-[18px]">I n</div>
+                <div v-if="achievement" class="text-[18px]">{{ achievement.department.name }}</div>
             </div>
             <div class="flex mt-[3vw] mb-[5vw] flex-col items-center">
                 <hr width="70%">
             </div>
             <div class="flex flex-col mx-[20%] text-[#CCCCCC]">
-                <div class="text-[25px]">Abteilung</div>
-                <div class="text-[18px]">Information</div>
+                <div class="text-[25px]">Standort</div>
+                <div v-if="achievement" class="text-[18px]">{{ achievement.location.name }} [{{ achievement.location.roomNumber }}]</div>
             </div>
             <div class="flex mt-[3vw] mb-[5vw] flex-col items-center">
                 <hr width="70%">
             </div>
             <div class="flex flex-col flex-wrap mx-[20%] text-[#CCCCCC]">
-                <div class="text-[25px]">Abteilung</div>
-                <div class="text-[18px] break-words">Information dvcgdvhcvhdsvchdv chdsvchdshcjh</div>
+                <div class="text-[25px]">Beschreibung</div>
+                <div v-if="achievement" class="text-[18px] break-words">{{ achievement.description }}</div>
             </div>
             <div class="flex mt-[3vw] mb-[5vw] flex-col items-center">
                 <hr width="70%">

32498c55f1f97bb2f94652f8b09a86ad6fdd1115
JakobDrescher <jakobdrescher6@gmail.com>
2024-01-09 14:53:43 +0100

    wrote the getAllAchievements function

--- PHPCode/src/UserAchievementController.php
+++ PHPCode/src/UserAchievementController.php
@@ -45,4 +45,30 @@ class UserAchievementController
         }
         return json_encode($result);
     }
+
+    public function getAllAchievements():string{
+        $result = array();
+        $sql = "SELECT a.pk_achievementID,a.name AS achievementName,d.pk_departmentID,d.name AS departmentName,a.description
+                FROM user u 
+                    JOIN userpossessesachievement up ON (u.pk_guID = up.pk_fk_guID) 
+                    JOIN achievement a ON (up.pk_fk_achievementID = a.pk_achievementID) 
+                    LEFT JOIN department d ON (d.pk_departmentID = a.fk_departmentID);";
+        $stmt = DB->prepare($sql);
+        $stmt->execute();
+        while ($row = $stmt->fetch()) {
+            $sql = "SELECT pk_locationID,roomNumber,name
+                    FROM achievementatlocation 
+                        JOIN location ON (pk_locationID=fk_locationID) 
+                    WHERE fk_achievementID = :achievementID";
+            $stmt2 = DB->prepare($sql);
+            $stmt2->bindValue(':achievementID', $row['pk_achievementID']);
+            $stmt2->execute();
+            $locations = array();
+            while ($row2 = $stmt2->fetch()) {
+                $locations[] = new Location($row2['pk_locationID'], $row2['roomNumber'], $row2['name']);
+            }
+            $result[] = new Achievement($row['pk_achievementID'], $row['achievementName'], "test", new Department($row['pk_departmentID'], $row['departmentName']), $locations, $row['description']);
+        }
+        return json_encode($result);
+    }
 }

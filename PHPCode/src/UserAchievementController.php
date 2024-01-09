0daff438bfa0e4826d3e47c069db288277c70174
JakobDrescher <jakobdrescher6@gmail.com>
2024-01-09 17:36:03 +0100

    wrote the function getAchievement but there is still an error thet keeps coming up which I have to figure out

--- PHPCode/src/UserAchievementController.php
+++ PHPCode/src/UserAchievementController.php
@@ -5,10 +5,10 @@ namespace Jdres\Tofting;
 
 require BASE_PATH.'/vendor/autoload.php';
 
-use Jdres\Tofting\User;
+use Jdres\Tofting\Achievement;
 use Jdres\Tofting\Department;
 use Jdres\Tofting\Location;
-use Jdres\Tofting\Achievement;
+use Jdres\Tofting\User;
 use PDO;
 
 //localhost might need to be replaced with another hostname when running on the live server
@@ -33,7 +33,7 @@ class UserAchievementController
             $sql = "SELECT pk_locationID,roomNumber,name
                     FROM achievementatlocation 
                         JOIN location ON (pk_locationID=fk_locationID) 
-                    WHERE fk_achievementID = :achievementID";
+                    WHERE fk_achievementID = :achievementID;";
             $stmt2 = DB->prepare($sql);
             $stmt2->bindValue(':achievementID', $row['pk_achievementID']);
             $stmt2->execute();
@@ -57,7 +57,33 @@ class UserAchievementController
             $sql = "SELECT pk_locationID,roomNumber,name
                     FROM achievementatlocation 
                         JOIN location ON (pk_locationID=fk_locationID) 
-                    WHERE fk_achievementID = :achievementID";
+                    WHERE fk_achievementID = :achievementID;";
+            $stmt2 = DB->prepare($sql);
+            $stmt2->bindValue(':achievementID', $row['pk_achievementID']);
+            $stmt2->execute();
+            $locations = array();
+            while ($row2 = $stmt2->fetch()) {
+                $locations[] = new Location($row2['pk_locationID'], $row2['roomNumber'], $row2['name']);
+            }
+            $result[] = new Achievement($row['pk_achievementID'], $row['achievementName'], "test", new Department($row['pk_departmentID'], $row['departmentName']), $locations, $row['description']);
+        }
+        return json_encode($result);
+    }
+
+    public function getAchievement(int $id):string{
+        $result = array();
+        $sql = "SELECT a.pk_achievementID, a.name AS achievementName, d.pk_departmentID, d.name AS departmentName, a.description 
+                FROM achievement a 
+                    JOIN department d on d.pk_departmentID = a.fk_departmentID;
+                WHERE a.pk_achievementID = :id;";
+        $stmt = DB->prepare($sql);
+        $stmt->bindValue(':id',$id);
+        $stmt->execute();
+        while ($row = $stmt->fetch()) {
+            $sql = "SELECT pk_locationID,roomNumber,name
+                    FROM achievementatlocation 
+                        JOIN location ON (pk_locationID=fk_locationID) 
+                    WHERE fk_achievementID = :achievementID;";
             $stmt2 = DB->prepare($sql);
             $stmt2->bindValue(':achievementID', $row['pk_achievementID']);
             $stmt2->execute();
